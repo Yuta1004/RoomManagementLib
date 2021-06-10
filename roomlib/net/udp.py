18710446a45d3ba67a7bb40beff10b7845ad1b04
NakagamiYuta <guguru0014@gmail.com>
2021-06-10 22:28:42 +0900

    [add] UDPブロードキャストの送信/受信関数を実装

---
+++ roomlib/net/udp.py
@@ -0,0 +1,46 @@
+from ipaddress import ip_interface
+from socket import socket, timeout, AF_INET, SOCK_DGRAM, SOL_SOCKET, SO_BROADCAST
+
+
+def broadcast_send(address, subnet, port, msg):
+    """
+    指定ネットワークにUDPブロードキャストを流す
+
+    ## Params
+    - address : 指定するネットワークに属する任意のホストのアドレス(文字列)
+    - subnet : サブネット(xxx.xxx.xxx.xxx)
+    - port : ポート番号
+    """
+
+    target_address = str(ip_interface(address+"/"+str(subnet)).network.broadcast_address)
+
+    sock = socket(AF_INET, SOCK_DGRAM)
+    sock.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+
+    sock.sendto(msg.encode(), (target_address, port))
+    sock.close()
+
+
+def broadcast_recv(port):
+    """
+    UDPブロードキャストを受信する
+
+    ## Params
+    - port : ポート番号
+
+    ## Return
+    - msg : 受信したメッセージ
+    - address : 送信元ホストのアドレス
+    (※タイムアウトしたなら両方None)
+    """
+
+    sock = socket(AF_INET, SOCK_DGRAM)
+    sock.bind(("", port))
+
+    msg, address = None, None
+    try:
+        msg, address = sock.recvfrom(4096)
+    except:
+        pass
+    sock.close()
+    return msg, address
