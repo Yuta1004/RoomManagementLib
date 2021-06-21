5140a75c4d32a0bd83f27c00bf84931a2c72b141
NakagamiYuta <guguru0014@gmail.com>
2021-06-22 02:40:17 +0900

    [update] example.py

--- example.py
+++ example.py
@@ -1,65 +1,66 @@
-from roomlib import Host
+import time
 
-host = Host(NAME, PORT, USERS, PASSWORD)
-
-while True:
-    if host.wait(TICK, PORT):
-        break
-
-host.get_value(KEY)
-
-host.set_values(KEY1=VALUE1, KEY2=VALUE2, ... , KEYn=VALUEn)
-
-host.sync()
-
-host.send(VALUE, [USERID])
-
-host.finish()
+from roomlib import Host, Client
 
 
-####
+def host_mode():
+    # 1. Host初期化
+    host = Host("ExampleRoom", 50000, 4, "example")
+
+    #2 . ユーザ参加待機
+    while True:
+        if host.wait(2.0, 50001):
+            break
+        print("waiting...")
+    print("New Client joined! : {}".format(host.user_list))
+
+    # 3. 通信を行う
+    print("Host >> hello, how are you?")
+    host.set_values(text="hello, how are you?")
+    host.sync()
+    time.sleep(2)
+    print("Client >> {}".format(host.get_value("text")))
+
+    # 4. 部屋を解散する
+    host.finish()
+    print("Bye...")
 
 
-from roomlib import Client
+def client_mode():
+    # 1. Client初期化
+    client = Client(50002)
 
-client = Client(PORT)
+    # 2. 部屋検索
+    room_id = ""
+    while True:
+        rooms = client.search(2.0, 50001)
+        if len(rooms.keys()) > 0:
+            room_id = list(rooms.keys())[0]
+            break
+        print('searching...')
 
-rooms = client.search(TICK, PORT)
-status = client.join(ROOMID, PASSWORD)
+    # 3. 部屋参加
+    if client.join(room_id, "example"):
+        print("Room joined! : {}".format(room_id))
+    else:
+        print("Failed to join")
 
-client.get_value(KEY)
+    # 4. 通信を行う
+    time.sleep(2)
+    print("Host >> {}".format(client.get_value("text")))
+    print("Client >> i'm fine!")
+    client.set_values(text="i'm fine!")
+    client.sync()
+    time.sleep(2)
 
-client.set_values(KEY1=VALUE1, KEY2=VALUE2, ..., KEYn=VALUEn)
-
-client.sync()
-
-client.finish()
+    # 5. ホストが部屋を解散する or 自分から退出する
+    # client.finish()
+    print("Is room available ? => {}".format(client.is_alive()))
 
 
-####
-
-## Request Format
-{
-   "command": "join/finish/sync",
-   "user": {
-       "id": "AAAAA",
-       "port": 11111
-   },
-   "auth": {
-       "password": ""
-   },
-   "values": {
-       "key": "value"
-   }
-}
-
-## Response Format
-{
-    "command": "AAAAA"
-    "result": {
-        "status": True or False,
-        "msg": "AAAAA"
-    }
-}
-
-###
+if __name__ == "__main__":
+    num = input("1: Host, 2 : Client : ")
+    if num == "1":
+        host_mode()
+    else:
+        client_mode()
